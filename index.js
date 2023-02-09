78e79ea7c709f62ac8554f8a4844efaa209e6a25
Nfaúndez <nat.faundezc@gmail.com>
2023-02-09 14:35:34 -0300

    larutaexiste

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-module.exports = () => {
-  // ...
-};
+const {
+  pathExists,
+} = require('./funciones');
+
+
+const mdLinks = (path, options) => new Promise((resolve, reject) => {
+  // Existe la ruta?
+  if (!pathExists(path)) {
+    // No existe la ruta, rechaza la promesa
+    reject(new Error('la ruta no existe'));
+    reject(new Error(`La ruta ${path} no existe`));
+  } else {
+  };
+});
---
+++ probandoNode.js
@@ -0,0 +1,3 @@
+const path = require('path');
+console.log('hola node');
+console.log(path.isAbsolute('/index.js'));
